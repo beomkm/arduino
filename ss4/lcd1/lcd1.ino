a36fd2ccda7d1085a748864b19395abba5fb985c
Beom-yeong Kim <tibyte@naver.com>
2017-03-30 12:05:27 +0900

    Add files via upload

---
+++ ss4/lcd1/lcd1.ino
@@ -0,0 +1,27 @@
+#include <LiquidCrystal.h>
+
+LiquidCrystal lcd(8,9,4,5,6,7);
+int len;
+
+void setup() {
+  lcd.begin(16, 2);
+  lcd.clear();
+  pinMode(10, OUTPUT);
+  digitalWrite(10, HIGH);
+  len = 1;
+}
+
+void loop() {
+  int i;
+  
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  for(i=0; i<len; i++) lcd.print("*");
+  lcd.setCursor(0, 1);
+  for(i=0; i<len%16+1; i++) lcd.print("*");
+
+  ++len;
+  if(len > 16) len = 1;
+  
+  delay(200);
+}
---
+++ ss4/motor1/motor1.ino
@@ -0,0 +1,19 @@
+int arr[] = {8, 9, 10, 11};
+
+void setup() {
+  int i;
+  for(i=0; i<4; i++) pinMode(arr[i], OUTPUT);
+  for(i=0; i<4; i++) digitalWrite(arr[i], LOW); 
+  digitalWrite(arr[0], HIGH);
+}
+
+void loop() {
+  int i;
+  for(i=0; i<4; i++) {
+    digitalWrite(arr[(i+3)%4], LOW);
+    delay(5);
+    digitalWrite(arr[(i+1)%4], HIGH);
+    delay(5);
+  }
+
+}
