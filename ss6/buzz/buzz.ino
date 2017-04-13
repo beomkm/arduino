192204bfc3069a56630e51073d90dec2e59e648d
Beom-yeong Kim <tibyte@naver.com>
2017-04-13 11:30:24 +0900

    Add files via upload

---
+++ ss6/buzz/buzz.ino
@@ -0,0 +1,17 @@
+#include "pitches.h"
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(A0, INPUT);
+}
+
+void loop() {
+
+  int data = analogRead(A0);
+  if(data < 10) {
+    tone(8, NOTE_C4);
+    delay(200);
+    noTone(8);
+  }
+ 
+}
---
+++ ss6/cds/cds.ino
@@ -0,0 +1,37 @@
+int flag = 1;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(2, INPUT);
+  pinMode(A0, INPUT);
+}
+
+void loop() {
+  int i;
+  int data;
+  int sum;
+  float voltage;
+  if(digitalRead(2) == LOW) {
+    if(flag) {
+      flag = 0;
+      sum = 0;
+      for(i=0; i<5; i++) {
+        data = analogRead(A0);
+        sum += data;
+        delay(100);
+      }
+      if(sum == 0)
+        voltage = 0;
+      else
+        voltage = (float)sum/5/1024*3.3;
+      Serial.print(voltage, 2);
+      Serial.println("V");
+    }
+  }
+  else {
+    flag = 1;
+  }
+  
+
+ 
+}
