4191c65474b0aa56d2f7ebc3dcd184594db6111b
Beom-yeong Kim <tibyte@naver.com>
2017-05-11 11:34:55 +0900

    Add files via upload

---
+++ ss8_timer/timer7seg/timer7seg.ino
@@ -0,0 +1,71 @@
+#include <TimerOne.h>
+
+int seg[][8] = {{1, 1, 1, 1, 1, 1, 0, 0}, //0
+                {0, 1, 1, 0, 0, 0, 0, 0}, //1
+                {1, 1, 0, 1, 1, 0, 1, 0}, //2
+                {1, 1, 1, 1, 0, 0, 1, 0}, //3
+                {0, 1, 1, 0, 0, 1, 1, 0}, //4
+                {1, 0, 1, 1, 0, 1, 1, 0}, //5
+                {1, 0, 1, 1, 1, 1, 1, 0}, //6
+                {1, 1, 1, 0, 0, 0, 0, 0}, //7
+                {1, 1, 1, 1, 1, 1, 1, 0}, //8
+                {1, 1, 1, 1, 0, 1, 1, 0}}; //9
+int segPins[] = {2, 3, 4, 5, 6, 7, 8, 9};
+int selPins[] = {10, 11, 12, 13};
+int count = 0;
+
+void setup() {
+  int i;
+  for(i=0; i<8; i++) pinMode(segPins[i], OUTPUT);
+  for(i=0; i<4; i++) pinMode(selPins[i], OUTPUT);
+  for(i=0; i<8; i++) digitalWrite(segPins[i], LOW);
+  for(i=0; i<4; i++) digitalWrite(selPins[i], HIGH);
+  Timer1.initialize(10000);
+  Timer1.attachInterrupt(handler);
+}
+
+void loop() {
+  int i, j;
+  int digit;
+
+  for(i=0; i<4; i++) {
+    //set
+    digitalWrite(selPins[i], LOW);
+
+    digit = (int)(count/pow10(i)) % 10;
+    for(j=0; j<7; j++) {
+      digitalWrite(segPins[j], seg[digit][j]);
+    }
+
+    //dot
+    if(i==2) digitalWrite(segPins[7], HIGH);
+    else digitalWrite(segPins[7], LOW);
+
+    //delay 2.5ms
+    delayMicroseconds(2500);
+    
+    //clear
+    for(j=0; j<8; j++) digitalWrite(segPins[j], LOW);
+
+    //release
+    digitalWrite(selPins[i], HIGH);
+  }
+}
+
+void handler() {
+  count += 1;
+  //00.00s ~ 59.99s
+  if(count > 5999) {
+    count = 0;
+  }
+}
+
+int pow10(int n) {
+  int i;
+  int ret = 1;
+  for(i=0; i<n; i++) {
+    ret *= 10;  
+  }
+  return ret;
+}
+
---
+++ ss8_timer/timerdotmat/timerdotmat.ino
@@ -0,0 +1,44 @@
+#include <TimerOne.h>
+
+int led[][8] = {{1, 1, 1, 1, 1, 1, 1, 1}, 
+                {1, 1, 1, 1, 1, 1, 1, 1}, 
+                {1, 1, 1, 1, 1, 1, 1, 1}, 
+                {1, 1, 1, 0, 0, 1, 1, 1}, 
+                {1, 1, 0, 0, 0, 0, 1, 1}, 
+                {1, 0, 0, 1, 1, 0, 0, 1}, 
+                {0, 0, 1, 1, 1, 1, 0, 0}, 
+                {0, 1, 1, 1, 1, 1, 1, 0}};
+int rowPin[] = {2, 3, 4, 5, 6, 7, 8, 9};
+int colPin[] = {14, 15, 16, 17, 18, 19, 20, 21};
+int dy = 0;
+                    
+void setup() {
+  int i;
+  for(i=0; i<8; i++) pinMode(rowPin[i], OUTPUT);
+  for(i=0; i<8; i++) pinMode(colPin[i], OUTPUT);
+  for(i=0; i<8; i++) digitalWrite(rowPin[i], LOW);
+  for(i=0; i<8; i++) digitalWrite(colPin[i], HIGH);
+  Timer1.initialize(300000);
+  Timer1.attachInterrupt(handler);
+}
+
+void loop() {
+  int i, j;
+  for(i=0; i<8; i++) {
+    digitalWrite(rowPin[i], HIGH);
+    for(j=0; j<8; j++) {
+      digitalWrite(colPin[j], led[(i+dy)%8][j]);
+    }
+    delayMicroseconds(1250);
+    digitalWrite(rowPin[i], LOW);
+  }
+}
+
+
+void handler() {
+  dy = (dy+1) % 8;
+}
+
+
+
+
